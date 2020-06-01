556b227cc374c35f68bab7290af514424e5ec1ba
pkesharwani <pkesharwani@hotwire.com>
2020-06-01 16:49:02 +0530

    INSERTION SORT

---
+++ Algorithms/Sorting/InsertionSort.js
@@ -0,0 +1,14 @@
+let insertionSort = (inputArr) => {
+    let len = inputArr.length;
+    for (let i = 1; i < len; i++) {
+            let j = i-1;
+            const min = inputArr[i];
+            while(j>=0 && inputArr[j] > min){
+              inputArr[j+1] = inputArr[j];
+              j=j-1; 
+            }
+            inputArr[j+1] = min;
+        }
+    return inputArr;
+  };
+  console.log(insertionSort([4,6,1,3,11,0,-2,2]))
