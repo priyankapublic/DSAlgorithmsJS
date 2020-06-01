d5ba078da1790bcf4f01019dcd957e7cd91aef4d
Priyanka <sweeti270789@gmail.com>
2020-06-01 17:10:54 +0530

    Enhancements

--- Algorithms/Sorting/BubbleSort.js
+++ Algorithms/Sorting/BubbleSort.js
@@ -1,18 +1,26 @@
-let bubbleSort = (inputArr) => {
-    let len = inputArr.length;
-    let swapped;
-    do {
-        swapped = false;
-        for (let i = 0; i < len; i++) {
-            if (inputArr[i] > inputArr[i + 1]) {
-                let tmp = inputArr[i];
-                inputArr[i] = inputArr[i + 1];
-                inputArr[i + 1] = tmp;
-                swapped = true;
-            }
-        }
-    } while (swapped);
-    return inputArr;
+let bubbleSort = inputArr => {
+  let len = inputArr.length;
+  let swapped;
+  do {
+    swapped = false;
+    for (let i = 0; i < len; i++) {
+      if (inputArr[i] > inputArr[i + 1]) {
+        let tmp = inputArr[i];
+        inputArr[i] = inputArr[i + 1];
+        inputArr[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return inputArr;
 };
-
-console.log(bubbleSort([4,6,1,3,11,0,-2,2]))
+document.getElementById("BubbleSort").innerHTML = bubbleSort([
+  4,
+  6,
+  1,
+  3,
+  11,
+  0,
+  -2,
+  2
+]);
