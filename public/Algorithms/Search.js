bf1deeab215c6c205fd53c1a51a5da856032e2c9
Priyanka <sweeti270789@gmail.com>
2021-01-29 11:52:51 +0530

    Search

--- public/Algorithms/Search.js
+++ public/Algorithms/Search.js
@@ -32,3 +32,17 @@ function binarySearch2(arr, elem) {
 
 console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103));
 console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 30));
+
+function naiveString(ls, ss) {
+  let count = 0;
+  let i = 0;
+  let j = 0;
+  for (i = 0; i < ls.length; i++) {
+    for (j = 0; j < ss.length; j++) if (ls[i + j] !== ss[j]) break;
+    if (j === ss.length) count++;
+  }
+
+  return count;
+}
+
+console.log(naiveString("wozomgwozomg", "omg"));
